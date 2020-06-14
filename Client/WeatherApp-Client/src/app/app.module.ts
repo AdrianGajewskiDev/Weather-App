4507cbebaab2eec2d9f8bdab81c13322dcf0d4d2
adrian.gajewskiDev@outlook.com <adrian.gajewskiDev@outlook.com>
2020-06-14 18:31:04 +0200

    add home component

--- Client/WeatherApp-Client/src/app/app.module.ts
+++ Client/WeatherApp-Client/src/app/app.module.ts
@@ -1,16 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
 
-import { AppComponent } from './app.component';
+import { routes } from "../app/app.routes";
+
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home-component/home-component";
+import { from } from "rxjs";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, HomeComponent],
+  imports: [BrowserModule, RouterModule.forRoot(routes)],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
