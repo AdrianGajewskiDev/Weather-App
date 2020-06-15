28377f7e585dab51ede2d4ca35201eebb093d414
adrian.gajewskiDev@outlook.com <adrian.gajewskiDev@outlook.com>
2020-06-15 17:18:46 +0200

    style the home component

--- Client/WeatherApp-Client/src/app/app.module.ts
+++ Client/WeatherApp-Client/src/app/app.module.ts
@@ -1,16 +1,42 @@
 import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
+import { ReactiveFormsModule } from "@angular/forms";
+import { FormsModule } from "@angular/forms";
+
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { MatCardModule } from "@angular/material/card";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
+import { MatDividerModule } from "@angular/material/divider";
+import { MatTooltipModule } from "@angular/material/tooltip";
+import { MatIconModule } from "@angular/material/icon";
 
 import { routes } from "../app/app.routes";
 
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./home-component/home-component";
-import { from } from "rxjs";
+import { from, fromEventPattern } from "rxjs";
 
 @NgModule({
   declarations: [AppComponent, HomeComponent],
-  imports: [BrowserModule, RouterModule.forRoot(routes)],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes),
+    FlexLayoutModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    MatTooltipModule,
+    MatDividerModule,
+    MatIconModule,
+    FormsModule,
+    MatInputModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- Client/WeatherApp-Client/src/app/home-component/home-component.ts
+++ Client/WeatherApp-Client/src/app/home-component/home-component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, Input } from "@angular/core";
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  FormControl,
+} from "@angular/forms";
+import { ThemePalette } from "@angular/material/core";
+import { TooltipPosition } from "@angular/material/tooltip";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-home-component",
@@ -6,7 +15,29 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./home-component.css"],
 })
 export class HomeComponent implements OnInit {
-  constructor() {}
+  constructor(private fb: FormBuilder) {}
+  @Input()
+  color: ThemePalette = "primary";
+  form: FormGroup;
+  positionOptions: TooltipPosition[] = [
+    "after",
+    "before",
+    "above",
+    "below",
+    "left",
+    "right",
+  ];
+  position = new FormControl(this.positionOptions[0]);
 
-  ngOnInit(): void {}
+  private externalURl = " http://bulk.openweathermap.org/sample/";
+
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      cityFormControll: [""],
+    });
+  }
+
+  goToCityIDs(): void {
+    document.location.href = this.externalURl;
+  }
 }
